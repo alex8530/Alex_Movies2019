58550f3f0e272debd8a10db8453c38dd1f2dbb19
Alex <aliabuharb8530@gmail.com>
2018-09-10 12:01:00 -0700

    secure api key&add build.gradle

--- app/src/main/java/com/example/noone/alex_movies2019/utils/Constant.java
+++ app/src/main/java/com/example/noone/alex_movies2019/utils/Constant.java
@@ -4,18 +4,15 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import com.example.noone.alex_movies2019.BuildConfig;
+
 /**
  * Created by NoOne on 9/8/2018.
  */
 
 public  class Constant {
     public static final String BASE_URL = "http://api.themoviedb.org/3/";
-    public static final String API_KEY = "27230e1667c419dd452ea128b62c8055";
+    public static final String API_KEY = BuildConfig.API_KEY;
     public static String MOVIE_ID;
     public static final String BASE_IMAGE_URL="http://image.tmdb.org/t/p/w185/";
-
-
-
-
-
 }
